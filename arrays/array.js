3ce138b3e0dcf5cc0e5345dc50393c64fafd362a
Ahmed Rahman <ahmedrahman@msn.com>
2025-01-13 15:20:42 +0000

    arrays

---
+++ arrays/array.js
@@ -0,0 +1,8 @@
+const blogPosts = [
+    "My first blog post",
+    "My second blog post",
+    "My third blog post",
+];
+
+const input = prompt("Enter Blog Post Number:"); 
+console.log(blogPosts[input - 1]);
